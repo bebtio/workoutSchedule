0ab81083bc136b811f8bd88d7148b377f4bf0703
bebtio <bebtio@gmail.com>
2025-11-30 19:20:48 -0600

    Forgot a function

--- main.lua
+++ main.lua
@@ -62,22 +62,3 @@ function love.draw()
 end
 
 
-function drawWorkoutBox( dayOfWeek, exercises, x, y, width, height )
-    local rw1 = width  - 2
-    local rh1 = height - 2
-    local offsetw = (width - rw1)/2.0
-    local offseth = (height - rh1)/2.0
-    local font = love.graphics.getFont()
-    local gw = font:getWidth(dayOfWeek)
-    local gh = font:getHeight(dayOfWeek)
-
-    love.graphics.rectangle("fill",x,y, width, height, curve, curve)
-    love.graphics.setColor(0,0,0)
-    love.graphics.rectangle("fill",x+offsetw,y+offseth,rw1,rh1, curve, curve)
-    love.graphics.setColor(1,1,1)
-    love.graphics.rectangle("fill",x,y+curve, width, 1)
-    love.graphics.print(dayOfWeek, x + (width / 2.0) - (gw / 2.0) , y + (curve / 2.0) - (gh/2.0))
-
-    width = drawExercises(exercises, x+10, y + curve)
-end
-
