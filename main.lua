056faa23884f2fd1d18f544f89e9a63008809466
bebtio <bebtio@gmail.com>
2025-11-30 19:19:50 -0600

    Split up the workout draw code into a separate file

--- main.lua
+++ main.lua
@@ -1,17 +1,34 @@
 json = require("json")
+require("workout")
 
 function love.load(arg)
-    file = io.open("workout.json", "rb")
-    jsonString = file:read("*a")
-    file:close()
-    workouts =  json.decode(jsonString)
+
+    if #arg == 0 then
+        workoutFile = "workout.json"
+    else
+        workoutFile = arg[1]
+    end
+
+    file = io.open(workoutFile, "rb")
+
+    workouts = nil
+    if file == nil then
+        print("Input file is " .. workoutFile .. " is nil. Exiting...")
+        love.event.quit()
+    else
+        jsonString = file:read("*a")
+        file:close()
+        workouts =  json.decode(jsonString)
+    end
+
 
     boxSpacing = 10
+    curve = 30
     x0 = 0
     y0 = 0
     numWorkouts, rw0, rh0 = getLongestExerciseString(workouts, love.graphics.getFont())
     rw0 = rw0 * 1.3
-    rh0 = rh0 * 1.3
+    rh0 = (rh0 + curve )  * 1.1
     daysOfWeek = {"Monday",  "Friday", "Saturday", "Sunday"}
     love.window.setMode(numWorkouts*(boxSpacing+rw0), rh0 + boxSpacing) 
     love.window.setTitle("Workout Schedule")
@@ -20,7 +37,6 @@ end
 function love.update(dt)
 
     if love.keyboard.isDown('s') then
-        print("SCREEN SHOT " .. love.filesystem.getSaveDirectory() )
         love.graphics.captureScreenshot(function(image)
             local path = love.filesystem.getWorkingDirectory()
             local file = io.open(path .. "/workout.png", "wb")
@@ -28,6 +44,7 @@ function love.update(dt)
             file:close()
         end)
 
+        love.event.quit()
     end
     
 end
@@ -50,7 +67,6 @@ function drawWorkoutBox( dayOfWeek, exercises, x, y, width, height )
     local rh1 = height - 2
     local offsetw = (width - rw1)/2.0
     local offseth = (height - rh1)/2.0
-    local curve = 30
     local font = love.graphics.getFont()
     local gw = font:getWidth(dayOfWeek)
     local gh = font:getHeight(dayOfWeek)
@@ -65,117 +81,3 @@ function drawWorkoutBox( dayOfWeek, exercises, x, y, width, height )
     width = drawExercises(exercises, x+10, y + curve)
 end
 
-function drawExercises(exercises, x, y)
-    local font = love.graphics.getFont()
-    local gh = font:getHeight()
-    local yOffset = 0
-
-    for k,v in pairs(exercises) do
-        if v.type == "SetsReps" then
-            drawExerciseSetsReps(v.name, v.sets, v.reps, x, y + yOffset )
-        elseif v.type == "Reps" then
-            drawExerciseReps(v.name, v.reps, x, y + yOffset)
-        elseif v.type == "BigSet" then
-            drawExerciseBigSet(v.exercises, v.sets, x, y + yOffset)
-        elseif v.type == "Spacebreak" then
-            -- Does nothing, just applies the yOffset.
-        elseif v.type == "Linebreak" then
-            love.graphics.rectangle("fill", x, y + yOffset - gh / 2.0, 10, 1)
-        elseif v.type == "Text" then
-            love.graphics.print(v.text, x, y + yOffset)
-        else
-            -- If the next thing selected wasn't anything valid, remove a yOffset so we don't have blank space.
-            yOffset = yOffset - gh
-        end
-        yOffset = yOffset + gh
-    end
-end
-
-function drawExerciseSetsReps(exercise, numSets, numReps, x, y)
-    love.graphics.print(exercise .. ": " .. numSets .. "x" .. numReps, x, y)
-end
-
-function drawExerciseReps(exercise, numReps, x, y)
-    love.graphics.print(exercise .. ": " .. numReps, x, y)
-end
-
-function drawExerciseBigSet(exercises, numSets, x, y)
-    local font = love.graphics.getFont()
-    local gh = font:getHeight()
-    local yOffset = 0
-    local largestWidth = 0
-
-    for k,v in pairs(exercises) do
-        local strLen = 0
-        exerciseStr = v.reps .. " " .. v.name
-        love.graphics.print(exerciseStr, x, y + yOffset)
-        strLen = font:getWidth(exerciseStr)
-
-        if strLen > largestWidth then
-            largestWidth = strLen
-        end
-
-        yOffset = yOffset + gh
-    end
-
-
-    -- Draw line from first exercise
-    love.graphics.rectangle("fill", x + largestWidth    , y + gh /2, 10, 1)
-    -- Draw the line from the last exercise
-    love.graphics.rectangle("fill", x + largestWidth     , y + gh * #exercises - gh/2, 11, 1)
-    -- Draw the line connecting the two.
-    love.graphics.rectangle("fill", x + largestWidth + 10   , y + gh /2, 1, gh * #exercises - gh )
-    -- Draw the midpoint line
-    love.graphics.rectangle("fill", x + largestWidth + 10, y  + (gh * #exercises)/2.0, 10, 1)
-    -- Draw the numSets 
-    love.graphics.print(numSets, x + largestWidth + 20, y + (gh * #exercises)/2.0 - gh /2.0)
-end
-
-function getLongestExerciseString(workoutData, font)
-    local maxWidth = 0
-    local maxHeight = 0
-    local numWorkouts = 0
-
-    for _, day in ipairs(workoutData.workout) do
-        local currentHeight = 0
-        local currentWidth = 0
-        numWorkouts = numWorkouts+1
-        for _, exercise in ipairs(day.exercises) do
-
-            -- This SetReps and Text are likely to be the longest strings so used those
-            -- to compute the width.
-            if exercise.type == "SetsReps" then
-                local sets = tostring(exercise.sets)
-                local reps = tostring(exercise.reps)
-                local str = exercise.name .. ": " .. sets .. " x " .. reps
-
-                local currentWidth = font:getWidth(str)
-
-                if currentWidth > maxWidth then
-                    maxWidth = currentWidth 
-                end
-
-                currentHeight = currentHeight + 1
-            elseif exercise.type == "Text" then
-                currentWdith = font:getWidth(exercise.text)
-                if currentWidth > maxWidth then
-                    maxWidth = currentWidth 
-                end
-
-                currentHeight = currentHeight + 1
-            elseif exercise.type == "BigSet" then
-                -- Big set exercises will always be stacked so measure how
-                -- many there are and add them to the running height.
-                currentHeight = currentHeight + #exercise.exercises
-            else
-                currentHeight = currentHeight + 1
-            end  
-
-            if currentHeight > maxHeight then
-                maxHeight = currentHeight
-            end
-        end
-    end
-
-    return numWorkouts, maxWidth, maxHeight * font:getHeight()
-end
---
+++ workout.lua
@@ -0,0 +1,125 @@
+
+function drawExercises(exercises, x, y)
+    local font = love.graphics.getFont()
+    local gh = font:getHeight()
+    local yOffset = 0
+
+    for k,v in pairs(exercises) do
+        if v.type == "SetsReps" then
+            drawExerciseSetsReps(v.name, v.sets, v.reps, x, y + yOffset )
+            yOffset = yOffset + gh
+        elseif v.type == "Reps" then
+            drawExerciseReps(v.name, v.reps, x, y + yOffset)
+            yOffset = yOffset + gh
+        elseif v.type == "BigSet" then
+            yOffset = yOffset + drawExerciseBigSet(v.exercises, v.sets, x, y + yOffset)
+        elseif v.type == "Spacebreak" then
+            -- Does nothing, just applies the yOffset.
+            yOffset = yOffset + gh
+        elseif v.type == "Linebreak" then
+            love.graphics.rectangle("fill", x, y + yOffset + gh / 2.0, 10, 1)
+            yOffset = yOffset + gh
+        elseif v.type == "Text" then
+            love.graphics.print(v.text, x, y + yOffset)
+            yOffset = yOffset + gh
+        else
+            -- If the next thing selected wasn't anything valid, remove a yOffset so we don't have blank space.
+            yOffset = yOffset - gh
+        end
+    end
+
+end
+
+function drawExerciseSetsReps(exercise, numSets, numReps, x, y)
+    love.graphics.print(exercise .. ": " .. numSets .. "x" .. numReps, x, y)
+end
+
+function drawExerciseReps(exercise, numReps, x, y)
+    love.graphics.print(exercise .. ": " .. numReps, x, y)
+end
+
+function drawExerciseBigSet(exercises, numSets, x, y)
+    local font = love.graphics.getFont()
+    local gh = font:getHeight()
+    local yOffset = 0
+    local largestWidth = 0
+
+    for k,v in pairs(exercises) do
+        local strLen = 0
+        exerciseStr = v.reps .. " " .. v.name
+        love.graphics.print(exerciseStr, x, y + yOffset)
+        strLen = font:getWidth(exerciseStr)
+
+        if strLen > largestWidth then
+            largestWidth = strLen
+        end
+
+        yOffset = yOffset + gh
+    end
+
+
+    -- Draw line from first exercise
+    love.graphics.rectangle("fill", x + largestWidth    , y + gh /2, 10, 1)
+    -- Draw the line from the last exercise
+    love.graphics.rectangle("fill", x + largestWidth     , y + gh * #exercises - gh/2, 11, 1)
+    -- Draw the line connecting the two.
+    love.graphics.rectangle("fill", x + largestWidth + 10   , y + gh /2, 1, gh * #exercises - gh )
+    -- Draw the midpoint line
+    love.graphics.rectangle("fill", x + largestWidth + 10, y  + (gh * #exercises)/2.0, 10, 1)
+    -- Draw the numSets 
+    love.graphics.print(numSets, x + largestWidth + 20, y + (gh * #exercises)/2.0 - gh /2.0)
+
+    return yOffset
+end
+
+function getLongestExerciseString(workoutData, font)
+    local maxWidth = 0
+    local maxHeight = 0
+    local numWorkouts = 0
+
+    for _, day in ipairs(workoutData.workout) do
+        local currentHeight = 0
+        local currentWidth = 0
+        numWorkouts = numWorkouts+1
+        for _, exercise in ipairs(day.exercises) do
+
+            local currentWidth = 0
+            -- This SetReps and Text are likely to be the longest strings so used those
+            -- to compute the width.
+            if exercise.type == "SetsReps" then
+                local sets = tostring(exercise.sets)
+                local reps = tostring(exercise.reps)
+                local str = exercise.name .. ": " .. sets .. " x " .. reps
+
+                currentWidth = font:getWidth(str)
+
+
+                currentHeight = currentHeight + 1
+            elseif exercise.type == "Text" then
+                currentWidth = font:getWidth(exercise.text)
+                currentHeight = currentHeight + 1
+            elseif exercise.type == "BigSet" then
+                -- Big set exercises will always be stacked so measure how
+                -- many there are and add them to the running height.
+
+                for _, exer in pairs( exercise.exercises ) do
+                    local str = exercise.sets .. " " .. exer.name .. " " .. exer.reps
+                    currentWidth = font:getWidth(str) + 50
+                end
+                currentHeight = currentHeight + #exercise.exercises
+            else
+                currentHeight = currentHeight + 1
+            end  
+
+            if currentWidth > maxWidth then
+                maxWidth = currentWidth 
+            end
+
+            if currentHeight > maxHeight then
+                maxHeight = currentHeight
+            end
+        end
+    end
+
+    return numWorkouts, maxWidth, maxHeight * font:getHeight()
+end
